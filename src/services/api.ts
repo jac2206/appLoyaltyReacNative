7016e8393fb8bf2af5d087f9b90f970045d393f1
jac2206 <arango773@gmail.com>
2026-04-07 00:42:01 -0500

    feat: add asyncstorage

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,12 @@
 import axios from "axios";
+// import { removeToken } from "./data/storage";
+import { Alert } from "react-native";
+import { removeItem } from "./data/storage.repository";
+import { STORAGE_KEYS } from "../constants/storageKeys";
+
+export const api = axios.create({
+  baseURL: "https://loyalty-backend-production-545b.up.railway.app/loyalty/v1",
+});
 
 let authToken: string | null = null;
 
@@ -6,15 +14,37 @@ export function setAuthToken(token: string) {
   authToken = token;
 }
 
-export const api = axios.create({
-  baseURL: "https://loyalty-backend-production-545b.up.railway.app/loyalty/v1",
-});
-
 api.interceptors.request.use((config) => {
-
   if (authToken) {
     config.headers.Authorization = `Bearer ${authToken}`;
   }
-
   return config;
 });
+
+let onUnauthorized: (() => void) | null = null;
+
+export function setUnauthorizedHandler(cb: () => void) {
+  onUnauthorized = cb;
+}
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+
+    if (error.response?.status === 401) {
+      console.log("Token expirado");
+
+      Alert.alert(
+        "Sesión expirada",
+        "Tu sesión ha expirado, por favor inicia sesión nuevamente"
+      );
+
+      // await removeToken();
+      await removeItem(STORAGE_KEYS.TOKEN);
+
+      onUnauthorized?.();
+    }
+
+    return Promise.reject(error);
+  }
+);
